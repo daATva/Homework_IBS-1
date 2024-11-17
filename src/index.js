6ab58e61eb93c68d7eaae7bd23c2fafdb079b6a1
daATva <riven1231@mail.ru>
2024-11-17 19:42:50 +0400

    feat: configured project assembly using Webpack

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import "../css/style.css";
+import "../css/product_in.css";
+import "./app.js";
+import "./product_in.js";
+import "./favorite.js";
+import "./filter.js";
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    clean: true,
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.html$/,
+        use: ["html-loader"],
+      },
+    ],
+  },
+  devServer: {
+    static: "./dist",
+    open: true,
+    port: 3000,
+  },
+};
